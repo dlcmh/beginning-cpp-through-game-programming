81c9525c70ed183ef7047924cd4bb5aaf21d5eca
David Chin <dlcmhd@me.com>
2017-03-19 22:56:03 +0800

    Add Score Rater 2.0

--- 010-score-rater-2.0/main.cpp
+++ 010-score-rater-2.0/main.cpp
@@ -1,36 +1,19 @@
-// Score Rater
-// Demonstrates the if statement
+// Score Rater 2.0
+// Demonstrates an else clause
 
 #include <iostream>
-#include <string>
-
 using namespace std;
 
 int main()
 {
-	if (true) {
-		cout << "This is always displayed.\n\n";
-	}
+	int score;
+	cout << "Enter your score: ";
+	cin >> score;
 
-	if (false) {
-		cout << "This is never displayed.\n\n";
-	}
-
-	int score = 1000;
-
-	if (score) {
-		cout << "At least you didn't score zero.\n\n";
-	}
-
-	if (score >= 250) {
-		cout << "You scored 250 or more. Decent.\n\n";
-	}
-
-	if (score >= 500) {
-		cout << "You scored 500 or more. Nice.\n\n";
-		if (score >= 1000) {
-			cout << "You scored 1000 or more. Impressive!\n";
-		}
+	if (score >= 1000) {
+		cout << "You scored 1000 or more. Impressive!\n";
+	} else {
+		cout << "You scored less than 1000.\n";
 	}
 
 	return 0;
