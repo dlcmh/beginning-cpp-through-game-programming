69ccde4c849715fd5cfdc8ac77637f2f373ff835
David Chin <dlcmhd@me.com>
2017-03-19 13:32:53 +0800

    Add Game Over 2.0

---
+++ 002-game-over-std-namespace/main.cpp
@@ -0,0 +1,11 @@
+// Game Over 2.0
+// Uses std namespace
+// Demonstrates using a directive
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Game Over!" << endl;
+	return 0;
+}
